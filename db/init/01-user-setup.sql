4acd90af005277e0f538b417ca4a3284df56e8f1
savayer <sava0094@gmail.com>
2025-03-24 22:38:04 +0400

    fixed deploy & mysql settings

--- db/init/01-user-setup.sql
+++ db/init/01-user-setup.sql
@@ -1,4 +1,11 @@
--- Разрешаем подключение пользователю telegram_bot с любого хоста
-CREATE USER IF NOT EXISTS 'telegram_bot'@'%' IDENTIFIED BY 'password';
+-- Сначала удаляем пользователя если он существует, чтобы избежать ошибок
+DROP USER IF EXISTS 'telegram_bot'@'%';
+
+-- Создаем пользователя с доступом от любого хоста (% wildcard)
+CREATE USER 'telegram_bot'@'%' IDENTIFIED BY 'password';
+
+-- Даем права только на базу данных telegram_bot (без GRANT OPTION для большей безопасности)
 GRANT ALL PRIVILEGES ON telegram_bot.* TO 'telegram_bot'@'%';
+
+-- Применяем изменения
 FLUSH PRIVILEGES; 
